41561392351099d331a658657733e3e21b539325
Dario Stromajer <115354821+DarioLoll@users.noreply.github.com>
2025-12-02 00:56:49 +0100

    Delete meaningless sample

--- samples/Mvvm.NestedNav.Avalonia.Samples/Routes/Routes.cs
+++
@@ -1,6 +0,0 @@
-namespace Mvvm.NestedNav.Avalonia.Samples.Routes;
-
-public record HomeRoute : Route;
-public record ProfileRoute : Route;
-public record SettingsRoute : Route;
-public record DetailsRoute(string Message) : Route;
--- samples/Mvvm.NestedNav.Avalonia.Samples/ViewModels/MainViewModel.cs
+++
@@ -1,8 +0,0 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Mvvm.NestedNav.Avalonia.Samples.ViewModels;
-
-public partial class MainViewModel(IViewModelFactory viewModelFactory) : ObservableObject
-{
-    [ObservableProperty] private IViewModelFactory _viewModelFactory = viewModelFactory;
-}
